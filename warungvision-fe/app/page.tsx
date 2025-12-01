7438ed310db04ab4ed4adc13f9f2e777d7decf50
krisnachandra <krisnachandra311@gmail.com>
2025-12-01 23:36:20 +0800

    first commit

---
+++ warungvision-fe/app/page.tsx
@@ -0,0 +1,10 @@
+import CameraView from "./components/CameraView";
+
+export default function Home() {
+  return (
+    <main>
+      <h1 className="text-center text-2xl font-bold py-4">Warung Vision 📸</h1>
+      <CameraView />
+    </main>
+  );
+}
